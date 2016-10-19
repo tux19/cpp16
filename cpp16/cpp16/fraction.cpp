4e3dab1406763ee22b20f32561c5ae828b16618d
Unknown <christian.ott@outlook.com>
2016-10-20 00:28:58 +0200

    fraction update

---
+++ cpp16/cpp16/fraction.cpp
@@ -0,0 +1,220 @@
+#include "stdafx.h"
+#include "fraction.h"
+#include <iostream>
+#include <exception>
+
+class myexception : public std::exception
+{
+	virtual const char* what() const throw()
+	{
+		return "ERROR: Devision by 0!";
+	}
+} division_by_zero;
+
+fraction::fraction(int c = 0, int d = 1) 
+	: cntr(c), denom(d)
+{
+	if(d == 0)
+	{
+		throw division_by_zero;
+	}
+	process_sign();
+	shorten_fraction();
+};
+
+fraction::~fraction()
+{
+}
+
+void fraction::process_sign()
+{
+	if( denom < 0 && cntr < 0)
+	{
+		denom = abs(denom);
+		cntr = abs(cntr);
+	}else if((denom < 0 && cntr > 0) || (denom > 0 && cntr < 0))
+	{
+		denom = abs(denom);
+		cntr = 0 - abs(cntr);
+	}
+}
+
+void fraction::shorten_fraction()
+{
+	auto tmp_gcd = gcd(abs(denom), abs(cntr));
+	denom /= tmp_gcd;
+	cntr /= tmp_gcd;
+}
+
+int fraction::gcd(int a, int b)
+{
+	return b == 0 ? a : gcd(b, a % b);
+}
+
+void fraction::set_counter(int cntr)
+{
+	fraction::cntr = cntr;
+	shorten_fraction();
+}
+
+int fraction::get_counter() const
+{
+	return fraction::cntr;
+}
+
+void fraction::set_denom(int denom)
+{
+	fraction::denom = denom;
+	shorten_fraction();
+}
+
+int fraction::get_denom() const
+{
+	return fraction::denom;
+}
+
+
+
+fraction fraction::operator+(const fraction rhs) const
+{
+	int nom = get_counter()*rhs.get_denom() + rhs.get_counter()*get_denom();
+	int denom = get_denom()*rhs.get_denom();
+
+	return fraction(nom, denom);
+}
+
+fraction fraction::operator-(const fraction rhs) const
+{
+	int nom = get_counter()*rhs.get_denom() - rhs.get_counter()*get_denom();
+	int denom = get_denom()*rhs.get_denom();
+
+	return fraction(nom, denom);
+}
+
+fraction fraction::operator*(const fraction rhs) const
+{
+	int nom = get_counter()*rhs.get_counter();
+	int denom = get_denom()*rhs.get_denom();
+
+	return fraction(nom, denom);
+}
+
+fraction fraction::operator/(const fraction f) const
+{
+	if(f.get_counter()==0)
+	{
+		throw division_by_zero;
+	}
+	int nom = get_counter()*f.get_denom();
+	int denom = get_denom()*f.get_counter();
+
+	return fraction(nom, denom);
+}
+
+fraction fraction::operator-() const
+{
+	return fraction(-cntr, denom);
+}
+
+fraction fraction::operator+(const int rhs) const
+{
+	return fraction(cntr, denom) + fraction(rhs);
+}
+
+fraction fraction::operator-(const int rhs) const
+{
+	return fraction(cntr, denom) - fraction(rhs);
+}
+
+fraction fraction::operator*(const int rhs) const
+{
+	return fraction(cntr, denom) * fraction(rhs);
+}
+
+fraction fraction::operator/(const int rhs) const
+{
+	return fraction(cntr,denom) / fraction(rhs);
+}
+
+fraction operator+(const int lhs, const fraction rhs)
+{
+	return fraction(lhs) + fraction(rhs.get_counter(),rhs.get_denom());
+
+}
+
+fraction operator-(const int lhs, const fraction rhs)
+{
+	return fraction(lhs) - fraction(rhs.get_counter(), rhs.get_denom());
+}
+
+fraction operator*(const int lhs, const fraction rhs)
+{
+	return fraction(lhs) * fraction(rhs.get_counter(), rhs.get_denom());
+}
+
+fraction operator/(const int lhs, const fraction rhs)
+{
+	return fraction(lhs) / fraction(rhs.get_counter(), rhs.get_denom());
+}
+
+bool fraction::operator==(const fraction rhs) const
+{
+	return (denom == rhs.get_denom() && cntr == rhs.get_counter());
+}
+
+bool fraction::operator!=(const fraction rhs) const
+{
+	return !(denom == rhs.get_denom() && cntr == rhs.get_counter());
+}
+
+bool fraction::operator<(const fraction rhs) const
+{
+	return (static_cast<double>(cntr / denom) < static_cast<double>(rhs.get_counter() / rhs.get_denom()));
+}
+
+bool fraction::operator>(const fraction rhs) const
+{
+	return (static_cast<double>(cntr / denom) > static_cast<double>(rhs.get_counter() / rhs.get_denom()));
+}
+
+bool fraction::operator<=(const fraction rhs) const
+{
+	return (static_cast<double>(cntr / denom) <= static_cast<double>(rhs.get_counter() / rhs.get_denom()));
+}
+
+bool fraction::operator>=(const fraction rhs) const
+{
+	return (static_cast<double>(cntr / denom) >= static_cast<double>(rhs.get_counter() / rhs.get_denom()));
+}
+
+inline void fraction::check_char(std::istream&is, char ch) {
+	char c;
+	is >> c;
+	if (c != ch)
+	{
+		is.putback(c);
+		is.setstate(std::ios::badbit);
+	}
+}
+
+std::istream& operator>>(std::istream& is, fraction& f) {
+	fraction g;
+	g.check_char(is, '(');
+	int temp;
+	is >> temp;
+	g.set_counter(temp);
+	g.check_char(is, '/');
+	is >> temp;
+	g.set_denom(temp);
+	g.check_char(is, ')');
+	if (is)
+	{
+		f = g;
+	}
+	return is;
+}
+
+std::ostream& operator<<(std::ostream& os, fraction f) {
+	os << '(' << f.get_counter() << '/' << f.get_denom() << ')';
+	return os;
+}
---
+++ cpp16/cpp16/fraction.h
@@ -0,0 +1,53 @@
+#include "stdafx.h"
+#include <iostream>
+
+class fraction
+{
+	
+private:
+	int cntr; 
+	int denom;
+
+	int gcd(int a, int b);
+	void shorten_fraction();
+	void process_sign();
+public:
+	fraction(int c, int d) ;
+	~fraction();
+
+	void set_counter(int cntr);
+	int get_counter() const;
+
+	void set_denom(int denom);
+	int get_denom()const;
+	
+	fraction operator-() const;
+
+	fraction operator+(const fraction rhs) const;
+	fraction operator-(const fraction rhs) const;
+	fraction operator*(const fraction rhs) const;
+	fraction operator/(const fraction rhs) const;
+
+	fraction operator+(const int rhs) const;
+	fraction operator-(const int rhs) const;
+	fraction operator*(const int rhs) const;
+	fraction operator/(const int rhs) const;
+
+	friend fraction operator+(const int lhs, const fraction rhs);
+	friend fraction operator-(const int lhs, const fraction rhs);
+	friend fraction operator*(const int lhs, const fraction rhs);
+	friend fraction operator/(const int lhs, const fraction rhs);
+
+	bool operator==(const fraction rhs) const;
+	bool operator!=(const fraction rhs) const;
+
+	bool operator<(const fraction rhs) const;
+	bool operator>(const fraction rhs) const;
+	bool operator<=(const fraction rhs) const;
+	bool operator>=(const fraction rhs) const;
+		
+	inline void check_char(std::istream&is, char ch);
+	friend std::istream& operator >> (std::istream& is, fraction& f);
+	friend std::ostream& operator<<(std::ostream& os, fraction f);
+};
+
