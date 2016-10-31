935f4fd4a03e9042d443272ba7f9f60ce44fd957
Headz0r <hh_reuzt_hh@hotmail.com>
2016-10-31 15:46:25 +0100

    spellchecker split into header and source, added with vectors , needs to be done with sped

--- cpp16/ex3/spellchecker.cpp
+++ cpp16/ex3/spellchecker.cpp
@@ -1,56 +1,71 @@
 #include "../ex2/pvector.h"
+#include "spellchecker.h"
 #include <iostream>
+#include <algorithm> //std::replace
+#include <vector>
 
 using namespace std;
-
-replace_word(string w,const string &newWord){}
-add_word(string w){}
-
-int main(int argc, char* argv[]){
-
-/*	if(argc < 2){
-		cout << "Example use:spellchecker.exe dictionary.txt text.txt" << endl;
-		return 0;
-	}
-*/
-	//Vector not working as intended
-/*	pvector<string> dict(argv[1]);
-	pvector<string> text(argv[2]);*/
-	
-	string dict[] = {"correctly", "spelled", "words", "are", "stored", "in", "this", "dictionary", "file"};
-	string text[] = {"A", "comprehensive", "dictionary", "is", "important"};
-	int i = 0;
-	int j = 0;
-
-	for(const string &word: text){ //Iterate through text
-		j=0;
+void replace_word(vector<string> tV,const string &w,const string &newWord){replace(tV.begin(),tV.end(),w,newWord);}
+void add_word(vector<string> dV,string w){dV.push_back(w);}
+void write_textcorrected(string dest){
+	dest.append(".corrected");
+}
+void check_dict(vector<string> dictV, vector<string> textV){
+	for(vector<string>::iterator word = textV.begin(); word != textV.end(); ++word){ 
 				bool inDict = false;
-				do{
-					if(dict[j].compare(word)== 0){
-						inDict = true;
-					}
-					j++;
-				}while(!inDict && j<9);
-				
+			for(vector<string>::iterator dictword = dictV.begin(); dictword != dictV.end(); ++dictword){
+				if(*dictword == *word){
+					
+					inDict = true;
+				 	break;
+				}
+			}
 			if(!inDict){
-				cout << word << endl;
+				cout << *word << endl;
 				cout << "(a)dd, (i)gnore, (r)eplace?";
 				string input;
 				getline(cin,input);
 				switch(input[0]){
 					case 'a':
-						add_word(word);
+						add_word(dictV,*word);
 					break;
 					case 'i':						
 					break;
 					case 'r':
 						string newWord;
 						getline(cin,newWord);
-						replace_word(word,newWord);
+						replace_word(textV,*word,newWord);
 					break;	
 				}
 			}
 		}
+		
+}
+
+
+int main(int argc, char* argv[]){
+
+/*	if(argc < 2){
+		cout << "Example use:spellchecker.exe dictionary.txt text.txt" << endl;
+		return 0;
+	}*/
+
+	//Vector not working as intended
+/*	pvector<string> dictV(argv[1]);
+	pvector<string> textV(argv[2]);*/
+	vector<string> dictV;
+	vector<string> textV;
+	//Temporary workaround
+	string dict[] = {"correctly", "spelled", "words", "are", "stored", "in", "this", "dictionary", "file"};
+	string text[] = {"A", "comprehensive", "dictionary", "is", "important"};
+	for(const string&word:text){
+		textV.push_back(word);
+	}
+	for(const string&word: dict){
+		dictV.push_back(word);
+	}
+	
+	check_dict(dictV, textV);
 	
 
 	
---
+++ cpp16/ex3/spellchecker.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+#ifndef SPELLCHECKER_H
+#define SPELLCHECKER_H
+void replace_word(std::vector<string> tV,const std::string &w,const std::string &newWord);
+void add_word(std::vector<string> dV,string w);
+void check_dict(std::vector<string> dictV, std::vector<string> textV);
+void write_textcorrected(std::string filename);
+#endif
+
