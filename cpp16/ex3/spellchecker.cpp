d5c415da35bbb2b86b7b6701ea0032e467fa4c49
Ra0R <reni95@live.de>
2016-11-03 00:09:14 +0100

    Update spellchecker.cpp

--- cpp16/ex3/spellchecker.cpp
+++ cpp16/ex3/spellchecker.cpp
@@ -1,3 +1,4 @@
+
 #include "spellchecker.h"
 #include <iostream>
 #include <fstream>
@@ -12,7 +13,7 @@ void add_word(set<string> *dV,string w){(*dV).insert(w);}
 
 void parse_cmdline(int argc, char *argv[])
 {
-    // whatever you want
+    
 }
 void loadtext(vector<std::string> *tV,std::string filename){
 	ifstream myReadFile;
@@ -30,17 +31,38 @@ void writetext(vector<std::string> tV,std::string const &filename){
  	 if (myfile.is_open()) {
 	 	for(vector<string>::iterator word = tV.begin(); word != tV.end(); ++word){ 
 	    myfile << *word<<" "; 
-	    cout << "Writing";
 		}
 		myfile.close();
 	}
 }
 
-void check_dict(set<string> dictV, vector<string> textV){
-	for(vector<string>::iterator word = textV.begin(); word != textV.end(); ++word){ 
+void loaddict(set<std::string> *tV,std::string filename){
+	ifstream myReadFile;
+	 myReadFile.open(filename);
+	 char output[100];
+	 if (myReadFile.is_open()) {
+		 while (!myReadFile.eof()) {
+	  	 myReadFile >> output;
+	    (*tV).insert(output);
+		}
+	}
+}
+void writedict(std::set<std::string> tV,std::string const &filename){
+	 ofstream myfile(filename);
+ 	 if (myfile.is_open()) {
+	 	for(set<string>::iterator word = tV.begin(); word != tV.end(); ++word){ 
+	    myfile << *word<<" "; 
+		}
+		myfile.close();
+	}
+}
+
+void check_dict(set<string> *dictV, vector<string> *textV){
+	for(vector<string>::iterator word = (*textV).begin(); word != (*textV).end(); ++word){ 
 				bool inDict = false;
-			for(set<string>::iterator dictword = dictV.begin(); dictword != dictV.end(); ++dictword){
+			for(set<string>::iterator dictword = (*dictV).begin(); dictword != (*dictV).end(); ++dictword){
 				if(*dictword == *word){
+					//cout << *dictword << " " << *word << endl;
 					inDict = true;
 				 	break;
 				}
@@ -52,19 +74,19 @@ void check_dict(set<string> dictV, vector<string> textV){
 				getline(cin,input);
 				switch(input[0]){
 					case 'a':
-						add_word(&dictV,*word);
+						add_word(&(*dictV),*word);
 					break;
 					case 'i':						
 					break;
 					case 'r':
 						string newWord;
 						getline(cin,newWord);
-						replace_word(&textV,*word,newWord);
+						replace_word(&(*textV),*word,newWord);
 					break;	
 				}
 			}
 		}
-		writetext(textV,"text.txt.corrected");
+	
 		
 }
 
@@ -75,16 +97,22 @@ int main(int argc, char* argv[]){
 		cout << "Example use:spellchecker.exe dictionary.txt text.txt" << endl;
 		return 0;
 	}
+	string filename1 = argv[1];//Dict
+	string filename2 = argv[2];//Text
 	vector<string> text;
 	set<string> dictS;
-	loadtext(&text,argv[2]);
-	string dict[] = {"correctly", "spelled", "words", "are", "stored", "in", "this", "dictionary", "file"};
+	loaddict(&dictS, filename1);
+	loadtext(&text,filename2);
+	/*string dict[] = {"correctly", "spelled", "words", "are", "stored", "in", "this", "dictionary", "file"};
 	for(const string&word: dict){
 		dictS.insert(word);
-	}
+	}*/
 
-	check_dict(dictS, text);
+	check_dict(&dictS, &text);
+		filename2 += ".corrected";
+	writetext(text,filename2);
 
+	writedict(dictS,filename1);
 	
 	
 	return 0;
