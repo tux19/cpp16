357c106b0f136f066d6580025299c11e7fd96cf5
Ra0R <reni95@live.de>
2016-11-02 19:49:35 +0100

    Update mergestl.cpp
    Implemented Merge for 2 vectors

--- cpp16/ex4/mergestl.cpp
+++ cpp16/ex4/mergestl.cpp
@@ -28,7 +28,26 @@ and container of same kind: merge(array,array);
 			    merge(list,list);
 
 */
+#include <iostream>
+#include <vector>
+template<typename T>
+typename std::vector<T> merge(typename std::vector<T>::iterator begin1,typename std::vector<T>::iterator end1,
+						typename std::vector<T>::iterator begin2,typename std::vector<T>::iterator end2){
+		std::vector<T> mergedV;
+		mergedV.insert( mergedV.end(), begin1, end1 );
+		mergedV.insert( mergedV.end(), begin2, end2 );
+		return mergedV;
+}
+
 int main(){
+	static const int arr[] = {1,2,3,4};
+	static const int arr2[] = {5,6,7,99};
+	std::vector<int> first (arr, arr + sizeof(arr) / sizeof(arr[0]) );
+	std::vector<int> second(arr2, arr2 + sizeof(arr2) / sizeof(arr2[0]) );
+	std::vector<int> third = merge<int>(first.begin(),first.end(),second.begin(),second.end());
 	
+	for (auto i = third.begin(); i != third.end(); ++i){
+    std::cout << *i << ' ';
+	}
 	return 0;
 }
