0588cda102c730c07ee08d2a6322001956f4fa4f
Ra0R <reni95@live.de>
2016-11-02 14:35:06 +0100

    Update mergestl.cpp
    Merge Concept

--- cpp16/ex4/mergestl.cpp
+++ cpp16/ex4/mergestl.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+/*
+Containers to consider:
+Sequentielle Containerklassen
+array
+vector
+list
+forward_list
+deque
+Assoziative Containerklassen
 
+unordered_map
+unordered_multimap
+unordered_multiset
+unordered_set
+map
+multimap
+multiset
+set
+Containeradapterklassen (keine C++ Standardbibliothek-Iteratoren)
+
+priority_queue
+queue
+stack
+Only allow to merge container with higher specialization into lower specialization:list merge(list,forward-list);
+											merge(vector,list);
+and container of same kind: merge(array,array); 
+			    merge(list,list);
+
+*/
 int main(){
 	
 	return 0;
