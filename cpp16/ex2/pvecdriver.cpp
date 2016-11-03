0ab2afcc436c3ed2acf3e28e051e7339aa25d10a
Ra0R <reni95@live.de>
2016-11-03 08:33:12 +0100

    Update pvecdriver.cpp

--- cpp16/ex2/pvecdriver.cpp
+++ cpp16/ex2/pvecdriver.cpp
@@ -4,8 +4,9 @@
 int main(){
 	pvector<int> sample= pvector<int>("pvec.txt");
 	int x = 42;
+	int y = 41;
 	sample.push_back(x);
-	
+	sample.push_back(y);
 	for (int n = 0; n < sample.size(); n++) {
             std::cout << '\t' << n << ". " << sample[n] << std::endl;
         }
